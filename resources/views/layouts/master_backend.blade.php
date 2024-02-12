a6e522c8d1dc5cc9498c144ea02fc3ce776ce9ae
omsap <satang.satang2022@gmail.com>
2024-02-12 13:59:45 +0700

    อัพเดท

--- resources/views/layouts/master_backend.blade.php
+++ resources/views/layouts/master_backend.blade.php
@@ -56,7 +56,7 @@
 
             <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
                 <div class="app-brand demo">
-                    <a href="index.html" class="app-brand-link">
+                    <a href="{{ url('/dashboard') }}" class="app-brand-link">
                         <span class="app-brand-logo demo">
                         <img src="{{ asset('backend/assets/img/logoicon.png') }}" alt="">
                         </span>
